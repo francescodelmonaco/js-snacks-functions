5773da7a5fe2cd63855536b8cc22cd13864c1ce1
francescodelmonaco <francescodelmonaco1999@gmail.com>
2025-01-16 12:42:35 +0100

    add: ex 02 code

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -4,10 +4,18 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+function arrayIniziali (x) {
+    let iniziali = [];
 
+    for (let i=0; i<x.length; i++) {
+        iniziali.push (x[i].charAt(0));
+    }
+
+    return console.log (iniziali);
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log (arrayIniziali (names));
 
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
