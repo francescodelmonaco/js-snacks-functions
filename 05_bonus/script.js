dd9c5eb14eef198afaa3e3bb6fe87d44659c706c
francescodelmonaco <francescodelmonaco1999@gmail.com>
2025-01-16 15:49:16 +0100

    add: ex 05 code

--- 05_bonus/script.js
+++ 05_bonus/script.js
@@ -6,13 +6,23 @@ e buonasera se è sera (oltre le 17)
 */
 
 const name = 'Mario';
+let orario = prompt(`Scrivi un'ora`, `22`);
 
 
 // Dichiara la funzione qui.
+function saluto (x) {
+    if (x>0 && x<=13 ) {
+        return console.log (`Buongiorno ${name}`)
+    } else if (x>13 && x<=17) {
+        return console.log (`Buon pomeriggio ${name}`)
+    } else {
+        return console.log (`Buonasera ${name}`)
+    }
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log (saluto (orario));
 
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
