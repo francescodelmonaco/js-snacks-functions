4a5861a6b76a25bd0f7cf2b8e2978563780def45
francescodelmonaco <francescodelmonaco1999@gmail.com>
2025-01-16 14:59:03 +0100

    add: ex 03 code

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -1,13 +1,25 @@
 /* Scrivi una funzione che accetti un'array di stringhe e una lettera e restituisca un array contenente solo le parole che iniziano con quella lettera */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
-
+let letter = prompt(`Inserisci una lettera dell'alfabeto`, `A`);
 
 // Dichiara la funzione qui.
+function letterArray (letter) {
+    let letterNames = [];
 
+    for (let i=0; i<names.length; i++) {
+
+        if (names[i].charAt(0).toLowerCase() === letter.toLowerCase()) {
+            letterNames.push(names[i]);
+        }
+
+    }
+
+    return letterNames;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log (letterArray (letter));
 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
