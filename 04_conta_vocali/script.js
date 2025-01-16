be4527f96aa7ddebd16fa69a146cc799356b1d7f
francescodelmonaco <francescodelmonaco1999@gmail.com>
2025-01-16 17:27:41 +0100

    add: ex 04 arrow function

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -5,7 +5,32 @@ const vocali = ["a", "e", "i", "o", "u"];
 
 // Dichiara la funzione qui.
 
-function controlloVocali (word) {
+// function controlloVocali (word) {
+//     let numeroVocali = 0;
+
+//     for (let i=0; i<word.length; i++) {
+
+//         for (let x=0; x<vocali.length; x++) {
+//             if (word[i] === vocali[x]) {
+//                 numeroVocali++;
+//             }
+//         }
+
+//     }
+
+//     return numeroVocali;
+// }
+
+// Invoca la funzione qui e stampa il risultato in console
+
+// console.log (controlloVocali (word));
+
+
+//Risultato atteso se si passa 'javascript': 3 (a, a, i)
+
+// ARROW FUNCTION
+
+const controlloVocali = (word) => {
     let numeroVocali = 0;
 
     for (let i=0; i<word.length; i++) {
@@ -18,12 +43,7 @@ function controlloVocali (word) {
 
     }
 
-    return numeroVocali;
+    return numeroVocali;  
 }
 
-// Invoca la funzione qui e stampa il risultato in console
-
 console.log (controlloVocali (word));
-
-
-//Risultato atteso se si passa 'javascript': 3 (a, a, i)
