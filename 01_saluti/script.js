f0ecda9f599f84511e02ce63945d75c55ed42d3f
francescodelmonaco <francescodelmonaco1999@gmail.com>
2025-01-16 12:15:06 +0100

    add: ex 01 code

--- 01_saluti/script.js
+++ 01_saluti/script.js
@@ -4,10 +4,12 @@ const userName = 'Mario';
 
 
 // Dichiara la funzione qui.
-
+function saluto () {
+    return console.log (`Ciao ${userName}`);
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log (saluto ());
 
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
